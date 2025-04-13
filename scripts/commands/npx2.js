4a7a7d5f285f645abfadb919c31d94c6a5477868
Mohammad Nayan <mohammadnayan1669@gmail.com>
2025-04-13 10:54:59 +0600

    Update npx2.js

--- scripts/commands/npx2.js
+++ scripts/commands/npx2.js
@@ -1,38 +1,57 @@
 const fs = require("fs");
+const request = require("request");  // Require the 'request' module
 module.exports = {
-  config:{
-	name: "npx2",
-        version: "1.0.1",
-        prefix: false,
-	permssion: 0,
-	credits: "nayan", 
-	description: "Fun",
-	category: "no prefix",
-	usages: "😒",
-        cooldowns: 5, 
-},
+	config: {
+		name: "npx2",
+		version: "1.0.1",
+		prefix: false,
+		permssion: 0,
+		credits: "nayan",
+		description: "Fun",
+		category: "no prefix",
+		usages: "😒",
+		cooldowns: 5,
+	},
 
-handleEvent: async function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-  const content = event.body ? event.body : '';
-  const body = content.toLowerCase();
-  const axios = require('axios')
-const media = (
-    await axios.get(
-      'https://i.imgur.com/Yc2atQe.mp4',
-      { responseType: 'stream' }
-    )
-  ).data;
+	handleEvent: async function({ api, event, client, __GLOBAL }) {
+		var { threadID, messageID } = event;
+		const content = event.body ? event.body : '';
+		const body = content.toLowerCase();
 
-	if (body.indexOf("Love")==0 || body.indexOf("❤️‍🔥")==0 || body.indexOf("💌")==0 || body.indexOf("💘")==0 || body.indexOf("💟")==0 || body.indexOf("I love u")==0 || body.indexOf("I love you")==0 || body.indexOf("valobashi")==0 || body.indexOf("Valobashi")==0 || body.indexOf("🖤")==0) {
-		var msg = {
+		
+		const media = await new Promise((resolve, reject) => {
+			request.get(
+				'https://i.imgur.com/Yc2atQe.mp4',
+				{ encoding: null },
+				(error, response, body) => {
+					if (error) {
+						reject(error);
+					} else {
+						resolve(body);
+					}
+				}
+			);
+		});
+
+		if (
+			body.indexOf("Love") == 0 ||
+			body.indexOf("❤️‍🔥") == 0 ||
+			body.indexOf("💌") == 0 ||
+			body.indexOf("💘") == 0 ||
+			body.indexOf("💟") == 0 ||
+			body.indexOf("I love u") == 0 ||
+			body.indexOf("I love you") == 0 ||
+			body.indexOf("valobashi") == 0 ||
+			body.indexOf("Valobashi") == 0 ||
+			body.indexOf("🖤") == 0
+		) {
+			var msg = {
 				body: "ভালোবাসা সুন্দর🖤",
-				attachment: media
-			}
-			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+				attachment: media,
+			};
+			api.sendMessage(msg, threadID, messageID);
+			api.setMessageReaction("🖤", event.messageID, (err) => {}, true);
 		}
 	},
-	start: function({ nayan }) {
-  }
-}
+	start: function({ nayan }) {},
+};
