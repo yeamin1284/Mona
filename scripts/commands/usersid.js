7ad1fd260191f16d0b71044d4dd88540f166fde1
Mohammad Nayan <mohammadnayan1669@gmail.com>
2025-04-16 18:24:02 +0600

    Update usersid.js

--- scripts/commands/usersid.js
+++ scripts/commands/usersid.js
@@ -1,18 +1,40 @@
 module.exports.config = {
-	name: "uid2",
-	version: "1.0.0",
-	permission: 0,
-	credits: "ryuko",
-	prefix: false,
-	description: "get user id.",
-	category: "without prefix",
-	cooldowns: 5
+  name: "uid2",
+  version: "1.1.0",
+  permission: 0,
+  credits: "Nayan",
+  prefix: false,
+  description: "Get UID only",
+  category: "without prefix",
+  cooldowns: 5
 };
 
-module.exports.run = function({ api, event }) {
-	if (Object.keys(event.mentions) == 0) return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
-	else {
-		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: ${Object.keys(event.mentions)[i]}`, event.threadID);
-		return;
-	}
-}
+module.exports.run = async function ({ api, event }) {
+  const content = event.body || "";
+
+  
+  const fbLinkMatch = content.match(/https?:\/\/(www\.)?facebook\.com\/[^\s]+/i);
+  if (fbLinkMatch) {
+    const link = fbLinkMatch[0];
+    try {
+      const uid = await new Promise((resolve, reject) => {
+        api.getUID(link, (err, result) => {
+          if (err) return reject(err);
+          resolve(result);
+        });
+      });
+      return api.sendMessage(`${uid}`, event.threadID, event.messageID);
+    } catch {
+      return api.sendMessage(``, event.threadID, event.messageID);
+    }
+  }
+
+  
+  if (Object.keys(event.mentions).length > 0) {
+    const uids = Object.keys(event.mentions).join('\n');
+    return api.sendMessage(uids, event.threadID, event.messageID);
+  }
+
+  
+  return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
+};
