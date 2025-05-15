a8bbe8fad575edeec9ada0f9bfdc278c1ab734f4
Mohammad Nayan <mohammadnayan1669@gmail.com>
2025-05-15 13:11:53 +0600

    Update tik.js

--- scripts/commands/tik.js
+++ scripts/commands/tik.js
@@ -22,7 +22,7 @@ start: async function({ nayan, events, args }) {
   const { messageID, threadID } = events;
 
   
-  const { tikdown } = require("nayan-videos-downloader")
+  const { tikdown } = require("nayan-videos-downloaders")
   const fs = require("fs");
   const axios = require("axios");
   const request = require("request");
