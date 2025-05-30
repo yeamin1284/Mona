c128d5d10f2370d25f2b35bab6e1f08f0b614dfc
Mohammad Nayan <mohammadnayan1669@gmail.com>
2025-05-30 16:40:17 +0600

    Update download.js

--- scripts/commands/download.js
+++ scripts/commands/download.js
@@ -38,7 +38,7 @@ start: async function ({ nayan, events, args, lang }) {
   const axios = require("axios");
   const fs = require("fs-extra");
   const content = args.join(" ");
-  const { ytdown, ndown, tikdown, twitterdown } = require("nayan-videos-downloaders")
+  const { ytdown, ndown, tikdown, twitterdown } = require("nayan-media-downloaders")
   let msg = "";
 
   const sendWaitingMessage = async (message) => {
